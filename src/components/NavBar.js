b5f9ff9f42fb1ab9751e3bab04965c9018a6dae7
calvinf <calvin.feau@gmail.com>
2021-01-27 18:12:20 -0500

    start styling set up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,15 +5,15 @@ import { ProjectContext } from "../context/ProjectContext";
 import { LayoutContext } from "../context/LayoutContext";
 
 const Wrapper = styled.div`
-z-index:                        100;
-align-items:                    center;
-height:                         10vh;
+z-index:                        100
 background-color:               transparent;
-font-size:                      24px;
 `;
-const Menu = styled.div`
-width:                          200px;
-`;
+// align-items:                    center;
+// height:                         10vh;
+// `;
+const Menu = styled.div``;
+// width:                          200px;
+;
 
 const NavBar = () => {
     const { resetImageToDisplay } = useContext(LayoutContext);
@@ -25,9 +25,13 @@ const NavBar = () => {
     };
 
     return ( 
-        <Wrapper className="width flex space-between">
-            <Link onClick={ () => handleClick() } to="/">Giselle Hernandez</Link>
-            <Menu className="flex space-between align-center"> 
+        <Wrapper className="width grid row-1">
+            <Link 
+            to="/"
+            className="text-large col-start-1"
+            onClick={ () => handleClick() } 
+            >Giselle Hernandez</Link>
+            <Menu className="col-start-10 flex space-between align-center"> 
                 <Link onClick={ () => handleClick() } to="/projects">Work</Link>
                 <Link onClick={ () => handleClick() } to="/about">About</Link>
             </Menu>
